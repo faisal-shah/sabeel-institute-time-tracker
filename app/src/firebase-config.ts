7cda7262650d1d047c52e40cf091ae416aab7e78
Faisal Shah <37458679+faisal-shah@users.noreply.github.com>
2026-07-15 17:58:34 -0500

    add firebase-config snippet from firebase

--- app/src/firebase-config.ts
+++ app/src/firebase-config.ts
@@ -8,3 +8,26 @@ export const firebaseConfig = {
   projectId: 'demo-sabeel',
   appId: '1:000000000000:web:placeholder',
 };
+
+
+// *******************************************************************
+// HERE IS THE CODE SNIPPET I GOT FROM FIREBASE
+// INTEGRATE INTO THE ABOVE AND THEN DELETE THIS AND EVERYTHING BELOW
+// *******************************************************************
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBw-htGUNKD_wIY5p6d_HTTJMzES5axWMI",
+  authDomain: "sabeel-institute-time-tracker.firebaseapp.com",
+  projectId: "sabeel-institute-time-tracker",
+  storageBucket: "sabeel-institute-time-tracker.firebasestorage.app",
+  messagingSenderId: "858585609550",
+  appId: "1:858585609550:web:eb1b0c69cee5248d145a1f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
